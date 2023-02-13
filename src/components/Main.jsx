c0fc1c0849df94345df2e31f7e4ca1f4914b7918
Amaralcode19 <lucasamaralap@outlook.com>
2023-02-13 11:15:41 -0300

    :hammer: add-slider

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,26 @@
-import { Tech}  from './Tech'
+import { Tech } from "./Tech";
+import { motion } from 'framer-motion';
+import { Slider } from "./Slider";
 
 export const Main = () => {
   return (
-    <div className="text-white not-italic flex flex-col justify-center h-[65vh] xs:w-full lg:w-5/6 mx-auto">
-      <div className='flex flex-col gap-24 xs:px-6 lg:px-0'>
-        <h1 className="text-2xl leading-[20px] font-extrabold">
+    <div className="flex flex-row h-[65vh] ">
+    <div className="text-white not-italic flex flex-col justify-center xs:w-full lg:w-5/6 mx-auto">
+      <div className="flex flex-col gap-24 xs:px-6 lg:px-0">
+        <motion.h1 animate={{x: 80}} className="text-2xl leading-[20px] font-extrabold">
           ☕ Hello World!
-        </h1>
+        </motion.h1>
         <p className="lg:w-1/4 xs:w-full font-normal leading-6 xs:text-base lg:text-xl">
-          Bem-vindo ao meu website, aqui você vai conhecer um pouco das minhas skills que tenho para oferecer e agregar ao mercado.
+          Bem-vindo ao meu website, aqui você vai conhecer um pouco das minhas
+          skills que tenho para oferecer e agregar ao mercado.
+        </p>
+        <p className="lg:w-1/4 xs:w-full font-normal leading-6 xs:text-base lg:text-xl">
+          Welcome, here you'll see a little bit about my sklls and what I had to
+          offer to the job market.
         </p>
       </div>
     </div>
+    <Slider />
+    </div>
   );
-}
+};
