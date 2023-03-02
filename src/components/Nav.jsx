79e386623bbe8364941ce07d4f73cf8c8200a359
Amaralcode19 <lucasamaralap@outlook.com>
2023-03-02 11:25:08 -0300

    :bulb: style-nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,12 @@ export const Nav = () => {
     return (
         <nav className="text-white flex justify-end">
             <ul className="flex justify-end space-x-10 p-6">
-                <li><a href="#sobre" className=" hover:text-neutral-300 ">Sobre</a></li>
-                <li><a href="#project" className=" hover:text-neutral-300">Projetos</a></li>
-                <li><a href="../archive/cv.pdf" download className="hover:text-neutral-300">Currículo</a></li>
+                <li><a href="#sobre" className="rounded-xl hover:p-2 hover:border-r-2 hover:border-b-2 hover:border-r-blue-700 hover:border-b-blue-600">Sobre</a></li>
+                <li><a href="#project" className="rounded-xl hover:p-2 hover:border-r-2 hover:border-b-2 hover:border-r-blue-700 hover:border-b-blue-600">Projetos</a></li>
+                <li><a href="../archive/cv.pdf" download className="rounded-xl hover:p-2 hover:border-r-2 hover:border-b-2 hover:border-r-blue-700 hover:border-b-blue-600">Currículo</a></li>
             </ul>
         </nav>
     )
-}
+};
 
 
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -16,13 +16,13 @@ export const Project = () => {
           id="project"
           className=" text-white flex xs:flex-col lg:flex-row justify-evenly xs:gap-14 lg:gap-0 align-center xs:w-full lg:w-5/6 xs:px-6 lg:px-0 mx-auto"
         >
-          <button className="bg-gradient-to-b from-blue-700 via-zinc-800 to-zinc-800 border-2 rounded-lg xs:py-16 xs:px-0 lg:px-24 lg:py-32 hover:scale-105 transition-all font-bold">
-            <a href="" target="_blank"></a>👨🏽‍💻 coming soon
+          <button className="bg-gradient-to-b from-blue-700 via-zinc-800 to-zinc-800 rounded-2xl xs:py-16 xs:px-0 lg:px-24 lg:py-32 hover:scale-105 transition-all font-bold">
+            <a href="" target="_blank"></a>🌊 - Wave <br /> working in progress...
           </button>
-          <button className="bg-gradient-to-b from-blue-700 via-zinc-800 to-zinc-800 border-2 rounded-lg xs:py-16 xs:px-0 lg:px-24 lg:py-32 hover:scale-105 transition-all font-bold">
+          <button className="bg-gradient-to-b from-blue-700 via-zinc-800 to-zinc-800 rounded-2xl xs:py-16 xs:px-0 lg:px-24 lg:py-32 hover:scale-105 transition-all font-bold">
             👨🏽‍💻 coming soon
           </button>
-          <button className="bg-gradient-to-b from-blue-700 via-zinc-800 to-zinc-800 border-2 rounded-lg xs:py-16 xs:px-0 lg:px-24 lg:py-32 hover:scale-105 transition-all font-bold">
+          <button className="bg-gradient-to-b from-blue-700 via-zinc-800 to-zinc-800 rounded-2xl xs:py-16 xs:px-0 lg:px-24 lg:py-32 hover:scale-105 transition-all font-bold">
             👨🏽‍💻 coming soon
           </button>
         </div>
