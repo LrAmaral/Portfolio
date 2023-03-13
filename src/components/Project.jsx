7cd9ab8c93e283cb592421aa4e31e07a8a84e138
Amaralcode19 <lucasamaralap@outlook.com>
2023-03-13 07:46:26 -0300

    :wrench: edit-scroll

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -17,11 +17,11 @@ export const Project = () => {
           className="flex flex-row text-white xs:flex-col lg:flex-row justify-evenly xs:gap-14 lg:gap-24 align-center xs:w-full lg:w-5/6 xs:px-6 lg:px-0 mx-auto"
         >
           <a href="https://lramaral.github.io/NLW---Setup" target="_blank" className="mb-8 w-fit flex flex-col items-center justify-center bg-gradient-to-b from-blue-800 via-zinc-800 to-zinc-800 rounded-3xl xs:py-6 xs:px-6 lg:px-24 lg:py-18 font-semibold">
-          <p className="mb-6 hover:underline hover:underline-offset-4">NLW SETUP - Explorer </p><img src="./explorer-1.jpg" className="rounded-xl" alt="explorer1"/>
+          <p className="mb-6 hover:underline hover:underline-offset-4">NLW SETUP - Explorer </p><img src="./explorer-1.jpg" className="rounded-xl hover:scale-105" alt="explorer1"/>
           <p className="mt-6">Habit Tracker Explorer by NLW's Rocketseat.</p></a>
 
          <a href="" className="mb-8 flex flex-col items-center justify-center bg-gradient-to-b from-blue-700 via-zinc-800 to-zinc-800 rounded-2xl xs:py-14 xs:px-0 lg:px-24 lg:py-32 font-bold">
-          <p className="mb-6 hover:underline hover:underline-offset-4">🌊 - Wave </p> <p className="mt-6"> working in progress...</p>
+          <p className="mb-6 hover:underline hover:underline-offset-4">🌊 - Wave </p> <p className="mt-6"> Working in progress...</p>
         </a>
         </div>
       </div>  
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -36,5 +36,5 @@ module.exports = {
       dropShadow: {
       },
     },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar")],
 }
